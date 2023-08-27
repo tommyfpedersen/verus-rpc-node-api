12c146e74cc9d289c60286873550c4a7130c69b5
Tommy Faldt Pedersen <tommy@faldt.net>
2023-08-27 14:15:03 +0200

    json return

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-/* express */
+/* verus node rpc username and password */
 require('dotenv').config();
-console.log(process.env.RPC_USERNAME);
-console.log(process.env.RPC_PASSWORD);
+const RPC_USERNAME = process.env.RPC_USERNAME; // <--- Change to your own verus node username 
+const RPC_PASSWORD = process.env.RPC_PASSWORD; // <--- Change to your own verus node password 
+
+/* express */
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const fetch = require('cross-fetch');
 const btoa = require('btoa');
-
-//const fetch = require('node-fetch');
-
-// const url = 'http://127.0.0.1:27486/';
-// const auth = 'Basic ' + Buffer.from(process.env.RPC_USERNAME+':'+process.env.RPC_PASSWORD).toString('base64');
-// const data = JSON.stringify({
-// jsonrpc: '1.0',
-// id: 'curltest',
-// method: 'getaddressbalance',
-// params: {
-// addresses: ['RCdXBieidGuXmK8Tw2gBoXWxi16UgqyKc7']
-// }
-// });
-// const headers = {
-// 'Content-Type': 'text/plain',
-// 'Authorization': auth
-// };
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -35,29 +20,28 @@ app.get('/getmininginfo', (req, res) => {
     method: 'POST',
     headers: {
       'content-type': 'text/plain;',
-      'Authorization': 'Basic ' + btoa('veruscoin:raspberrypi')
+      'Authorization': 'Basic ' + btoa(RPC_USERNAME + ':' + RPC_PASSWORD);
     },
-    body: '{"jsonrpc": "1.0", "id":"curltest", "method": "getaddressbalance", "params": [{"addresses": ["RCdXBieidGuXmK8Tw2gBoXWxi16UgqyKc7"]}] }'
+    body: '{"jsonrpc": "1.0", "id":"curltest", "method": "getmininginfo", "params": [{"addresses": ["RCdXBieidGuXmK8Tw2gBoXWxi16UgqyKc7"]}] }'
   })
     .then(response => {
-    if (response.ok) {
-    return response.json();
-    }
-    console.log({message:"Request failed!"});
-    res.json({message:"Request failed!"})
-   
-   // throw new Error('Request failed!');
+      if (response.ok) {
+        return res.json(response);
+      }
+      console.log({ message: "Request failed!" });
+      res.json({ message: "Request failed!" })
+
     })
     .then(jsonData => {
-    console.log(jsonData);
+      console.log(jsonData);
     })
     .catch(error => {
-    console.error(error);
+      console.error(error);
     });
- // res.send('getmininginfo')
+  // res.send('getmininginfo')
 })
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
+  console.log(`Example app listening on port ${PORT}`)
 })
 
