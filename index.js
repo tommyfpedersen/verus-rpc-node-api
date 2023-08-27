435434ff146bada4e2a87db1ed72847b1657a39a
Tommy Faldt Pedersen <tommy@faldt.net>
2023-08-27 14:00:38 +0200

    curl to fetch test

--- index.js
+++ index.js
@@ -10,31 +10,34 @@ const fetch = require('cross-fetch');
 
 //const fetch = require('node-fetch');
 
-const url = 'http://127.0.0.1:27486/';
-const auth = 'Basic ' + Buffer.from(process.env.RPC_USERNAME+':'+process.env.RPC_PASSWORD).toString('base64');
-const data = JSON.stringify({
-jsonrpc: '1.0',
-id: 'curltest',
-method: 'getaddressbalance',
-params: {
-addresses: ['RCdXBieidGuXmK8Tw2gBoXWxi16UgqyKc7']
-}
-});
-const headers = {
-'Content-Type': 'text/plain',
-'Authorization': auth
-};
+// const url = 'http://127.0.0.1:27486/';
+// const auth = 'Basic ' + Buffer.from(process.env.RPC_USERNAME+':'+process.env.RPC_PASSWORD).toString('base64');
+// const data = JSON.stringify({
+// jsonrpc: '1.0',
+// id: 'curltest',
+// method: 'getaddressbalance',
+// params: {
+// addresses: ['RCdXBieidGuXmK8Tw2gBoXWxi16UgqyKc7']
+// }
+// });
+// const headers = {
+// 'Content-Type': 'text/plain',
+// 'Authorization': auth
+// };
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/getmininginfo', (req, res) => {
-  fetch(url, {
+  fetch('http://127.0.0.1:27486/', {
     method: 'POST',
-    headers,
-    body: data
-    })
+    headers: {
+      'content-type': 'text/plain;',
+      'Authorization': 'Basic ' + btoa('veruscoin:raspberrypi')
+    },
+    body: '{"jsonrpc": "1.0", "id":"curltest", "method": "getaddressbalance", "params": [{"addresses": ["RCdXBieidGuXmK8Tw2gBoXWxi16UgqyKc7"]}] }'
+  })
     .then(response => {
     if (response.ok) {
     return response.json();
