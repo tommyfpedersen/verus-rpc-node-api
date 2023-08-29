84911d7403a5934ad92eda42e3c5a7429c057fc5
Tommy Faldt Pedersen <tommy@faldt.net>
2023-08-29 19:54:25 +0200

    config, routes and await

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    RPC_USERNAME: process.env.RPC_USERNAME, // <--- Change to your own verus node username
+    RPC_PASSWORD: process.env.RPC_PASSWORD, // <--- Change to your own verus node password 
+    VERUS_NODE_ADDRESS: 'http://127.0.0.1:27486/', // <--- Default ip and port number //http://127.0.0.1:27486/
+    ID: "verus-rest-api-v1.0.0"
+});
--- index.js
+++ index.js
@@ -1,96 +1,33 @@
-/* verus node rpc username and password */
 require('dotenv').config();
-const RPC_USERNAME = process.env.RPC_USERNAME; // <--- Change to your own verus node username 
-const RPC_PASSWORD = process.env.RPC_PASSWORD; // <--- Change to your own verus node password 
-const VERUS_NODE_ADDRESS = 'http://127.0.0.1:27486/'; // <--- Default ip and port number
-const ID = "verus-rest-api-v1.0.0";
 
 /* express */
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
+/* common */
 const fetch = require('cross-fetch');
 const btoa = require('btoa');
 
+/* routes */
+const addressindexRouter = require('./routes/addressindex');
+app.use('/addressindex', addressindexRouter);
 
+const miningRouter = require('./routes/mining');
+app.use('/mining', miningRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('verus rest api')
 })
-
-
-app.get('/getmininginfo', (req, res) => {
-  fetch(VERUS_NODE_ADDRESS, {
-    method: 'POST',
-    headers: {
-      'content-type': 'text/plain;',
-      'Authorization': 'Basic ' + btoa(RPC_USERNAME + ':' + RPC_PASSWORD)
-    },
-    body: '{"jsonrpc": "1.0", "id":"'+ID+'", "method": "getmininginfo", "params": [] }'
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      res.json({ message: "Request failed!" })
-    })
-    .then(data => {
-      res.json(data)
-    })
-    .catch(error => {
-      res.json({ message: "Request failed! - " + error })
-    });
-})
-
-app.get('/getaddressbalance', (req, res) => {
-  fetch(VERUS_NODE_ADDRESS, {
-    method: 'POST',
-    headers: {
-      'content-type': 'text/plain;',
-      'Authorization': 'Basic ' + btoa(RPC_USERNAME + ':' + RPC_PASSWORD)
-    },
-    body: '{"jsonrpc": "1.0", "id":"'+ID+'", "method": "getaddressbalance", "params": [{"addresses": ["RCdXBieidGuXmK8Tw2gBoXWxi16UgqyKc7"]}] }'
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      res.json({ message: "Request failed!" })
-    })
-    .then(data => {
-      res.json(data)
-    })
-    .catch(error => {
-      res.json({ message: "Request failed! - " + error })
-    });
-})
-
-app.get('/getdifficulty', (req, res) => {
-  fetch(VERUS_NODE_ADDRESS, {
-    method: 'POST',
-    headers: {
-      'content-type': 'text/plain;',
-      'Authorization': 'Basic ' + btoa(RPC_USERNAME + ':' + RPC_PASSWORD)
-    },
-    body: '{"jsonrpc": "1.0", "id":"'+ID+'", "method": "getdifficulty", "params": [] }'
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      res.json({ message: "Request failed!" })
-    })
-    .then(data => {
-      res.json(data)
-    })
-    .catch(error => {
-      res.json({ message: "Request failed! - " + error })
-    });
-})
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
 
+/* mongodb */
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.DATABASE_URL);
+// const db = mongoose.connection;
+// db.on('error',(error) => console.error(error));
+// db.once('open',() => console.log("Connected to Database"))
