c139e96554f7128231f5de0e0d08239cdddbee89
tommyfpedersen <tommy@faldt.net>
2023-11-14 17:26:44 +0100

    getcurrencystate and getimports added

--- routes/multichain.js
+++ routes/multichain.js
@@ -19,4 +19,38 @@ const router = express.Router();
     }
   })
 
+  router.get('/getcurrencystate/:chainname/:block', async (req, res) => {
+    try {
+        const response = await fetch(config.VERUS_NODE_ADDRESS, {
+            method: 'POST',
+            headers: {
+              'content-type': 'text/plain;',
+              'Authorization': 'Basic ' + btoa(config.RPC_USERNAME + ':' + config.RPC_PASSWORD)
+            },
+            body: '{"jsonrpc": "1.0", "id":"'+config.ID+'", "method": "getcurrencystate", "params": ["'+req.params.chainname+'", '+req.params.block+'] }'
+          });
+         const results = await response.json();
+         res.json(results);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+  })
+
+  router.get('/getimports/:chainname/:blockstart/:blockend', async (req, res) => {
+    try {
+        const response = await fetch(config.VERUS_NODE_ADDRESS, {
+            method: 'POST',
+            headers: {
+              'content-type': 'text/plain;',
+              'Authorization': 'Basic ' + btoa(config.RPC_USERNAME + ':' + config.RPC_PASSWORD)
+            },
+            body: '{"jsonrpc": "1.0", "id":"'+config.ID+'", "method": "getimports", "params": ["'+req.params.chainname+'", '+req.params.blockstart+', '+req.params.blockend+'] }'
+          });
+         const results = await response.json();
+         res.json(results);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+  })
+
 module.exports = router;
