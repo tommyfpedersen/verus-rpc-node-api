f122a254d474711be75e6ce54ffe050a55f97ebe
Tommy Faldt Pedersen <tommy@faldt.net>
2024-07-19 20:05:47 +0200

    extended getcurrencystate volume

--- routes/multichain.js
+++ routes/multichain.js
@@ -19,7 +19,7 @@ const router = express.Router();
     }
   })
 
-  router.get('/getcurrencystate/:chainname/:block', async (req, res) => {
+  router.get('/getcurrencystate/:chainname/:blockstart/:blockend?/:blockintervals?/:converttocurrency?', async (req, res) => {
     try {
         const response = await fetch(config.VERUS_NODE_ADDRESS, {
             method: 'POST',
@@ -27,7 +27,7 @@ const router = express.Router();
               'content-type': 'text/plain;',
               'Authorization': 'Basic ' + btoa(config.RPC_USERNAME + ':' + config.RPC_PASSWORD)
             },
-            body: '{"jsonrpc": "1.0", "id":"'+config.ID+'", "method": "getcurrencystate", "params": ["'+req.params.chainname+'", '+req.params.block+'] }'
+             body: '{"jsonrpc": "1.0", "id":"'+config.ID+'", "method": "getcurrencystate", "params": ["'+req.params.chainname+'", "'+req.params.blockstart+', '+req.params.blockend+', '+req.params.blockintervals+'","'+req.params.converttocurrency+'"] }'
           });
          const results = await response.json();
          res.json(results);
